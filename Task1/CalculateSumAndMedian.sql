bbf478f21592c0d0ca1d67b68954f6a2ddb6961e
westcrime <westcrime77@rambler.com>
2024-08-23 12:36:45 +0300

    made all in Task1

--- Task1/CalculateSumAndMedian.sql
+++ Task1/CalculateSumAndMedian.sql
@@ -1,14 +1,9 @@
 SELECT SUM(IntNumber) AS SumOfIntegers FROM Lines;
 
-SELECT * 
-FROM Lines 
-WHERE DoubleNumber = 
-(
-    SELECT AVG(DoubleNumber)
-    FROM (
-        SELECT DoubleNumber
-        FROM Lines
-        ORDER BY DoubleNumber
-        LIMIT 1 OFFSET (SELECT (COUNT(*) - 1) / 2 FROM Lines)
-    )
+SELECT AVG(DoubleNumber)
+FROM (
+    SELECT DoubleNumber
+    FROM Lines
+    ORDER BY DoubleNumber
+    LIMIT 1 OFFSET (SELECT (COUNT(*) - 1) / 2 FROM Lines)
 );
